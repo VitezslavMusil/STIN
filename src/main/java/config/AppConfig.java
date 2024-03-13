ea686a729e7dde174c2c4ce8e4efc88d0fa1d719
Vitezslav_Musil <vitezslav.musil@tul.cz>
2024-03-13 14:01:46 +0100

    cv02

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cz.tul.sti2024.cv")
+public class AppConfig {}
