4370210f069bd01208f3c183dbc625bf912a9a97
Vitezslav_Musil <vitezslav.musil@tul.cz>
2024-04-03 11:02:16 +0200

    Payment Services

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -4,5 +4,5 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("cz.tul.sti2024.cv")
+@ComponentScan()
 public class AppConfig {}
